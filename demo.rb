223885563e347988b8e0c6fddc04a3411d6c0e21
Colin Gross <grosscol@umich.edu>
2016-01-14 13:54:28 -0500

    config updates and add to demo.

--- demo.rb
+++ demo.rb
@@ -8,6 +8,7 @@ require 'active-fedora'
 ActiveFedora.init(:fedora_config_path => "config/fedora.yml")
 col = Hydra::PCDM::Collection.create
 obj = Hydra::PCDM::Object.create
+obj2 = Hydra::PCDM::Object.create
 file= Hydra::PCDM::File.new
 
 col.members << obj
@@ -16,4 +17,20 @@ col.save
 file = obj.files.build
 file.content = "The quick brown fox jumped over the lazy dog."
 obj.save
+
+col.ordered_members << obj
+col.ordered_members << obj
+col.ordered_members << obj2
+
+puts "ordered members"
+puts col.ordered_members
+
+puts "members"
+puts col.members
+
+puts "removing from ordered members"
+puts col.ordered_members.delete_at(0)
+
+puts "ordered members"
+puts col.ordered_members
 
